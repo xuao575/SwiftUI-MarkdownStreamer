2321fb2320158641ec5b9cf1b51cc26282b1c034
XU Ao <xuao575@gmail.com>
2025-05-07 23:19:07 +0800

    Initial Commit

---
+++ streamStyle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  streamStyle
+//
+//  Created by Ao XU on 5/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ streamStyle/streamStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  streamStyleApp.swift
+//  streamStyle
+//
+//  Created by Ao XU on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct streamStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
